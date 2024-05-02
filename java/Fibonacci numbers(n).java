9f565133a57d3ca7c013487e41c50800b6f499c2
coreashield <120443729+coreashield@users.noreply.github.com>
2024-05-02 16:39:30 +0900

    Update Fibonacci numbers(n).java

--- java/Fibonacci numbers(n).java
+++ java/Fibonacci numbers(n).java
@@ -1,15 +1,19 @@
 class Fibo {
     public static void main(String[] args) {
-        int n = 10; //
+        int n = 10;
+        int fiboArry = fiboValue(n);
+
+        // n번째 수열을 알고 싶으면 n-1번째 수열을 출력해야 한다.
+        System.out.println("fiboArry[n]: " + fiboArry);
+    }
+
+    public static int fiboValue(int n) {
         int[] fiboArry = new int[n];
         fiboArry[0] = 0;
         fiboArry[1] = 1;
-        for(int i = 2; i < n ; i++){
-            fiboArry[i] = fiboArry[i-1]+fiboArry[i-2];
+        for(int i = 2; i < n; i++){
+            fiboArry[i] = fiboArry[i-1] + fiboArry[i-2];
         }
-
-        System.out.println("fiboArry[n] " + fiboArry[n-1]);
+        return fiboArry[n-1];
     }
-
-    public static int
 }
