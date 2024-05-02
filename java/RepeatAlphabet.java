aecdab411fb295d85bf38521c90585cb6fa3dee0
coreashield <120443729+coreashield@users.noreply.github.com>
2024-05-02 16:34:54 +0900

    Update RepeatAlphabet.java

--- java/RepeatAlphabet.java
+++ java/RepeatAlphabet.java
@@ -2,15 +2,16 @@ class Repeat {
     public static void main(String[] args) {
         String a = "Happy";
         int n = 3;
-			repeatCharacters(a, n);
-        
+        System.out.println(repeatCharacters(a, n));
     }
 
-    public static void repeatCharacters(String str, int count) {
+    public static String repeatCharacters(String str, int count) {
+        String returnValue = "";
         for (int i = 0; i < str.length(); i++) {
             for (int j = 0; j < count; j++) {
-                 System.out.print(str.charAt(i);) 
+                returnValue = returnValue + str.charAt(i);
             }
         }
+        return returnValue;
     }
 }
