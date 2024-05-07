031aeba758874cb8c06428a4e499f1a70a51ccc0
coreashield <120443729+coreashield@users.noreply.github.com>
2024-05-07 17:09:31 +0900

    Create sum_evennumber.kts

---
+++ kotlin/sum_evennumber.kts
@@ -0,0 +1,21 @@
+fun main() {
+    var sum_value = 0
+    for (i in 1..100) {
+        if(i % 2 == 0){
+            //println(i) //짝수 출력
+            sum_value += i //짝수 합계
+        }
+    }
+    println(sum_value)
+}
+
+fun main() {
+    var sum_value = 0
+    for (i in 1..100){
+        when {
+            i % 2 == 0 -> sum_value += i
+            else -> continue
+        }
+    }
+    println(sum_value)
+}
